2395e6fde690269fe7562c589554d2f6adb6c0e2
BOTTANI Simona <simonabottani92@gmail.com>
2021-01-24 23:17:59 +0100

    import abc

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import torch
 import torchvision.transforms as transforms
 from torch.utils.data import Dataset
 import numpy as np
+import abc
 
 FILENAME_TYPE = {'full': '_T1w_space-MNI152NLin2009cSym_res-1x1x1_T1w',
                  'cropped': '_T1w_space-MNI152NLin2009cSym_desc-Crop_res-1x1x1_T1w',
