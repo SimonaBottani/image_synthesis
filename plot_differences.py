7f2ca3df0781ab8d9470c62f5076b735286cb69b
BOTTANI Simona <simonabottani92@gmail.com>
2021-02-08 14:00:34 +0100

    resolve

--- plot_differences.py
+++ plot_differences.py
@@ -10,9 +10,9 @@ real_image_path = (os.path.join('/export/home/cse180022/apprimage_simo/image_pre
                       'subjects', participant_id[i], 'ses-M000',
                                  't1_linear',participant_id[i] + '_' + 'ses-M000' + '_T1w_space-MNI152NLin2009cSym_res-1x1x1_T1w.nii.gz'))
 
-real_image = nib.load (real_image_path)
-header = nib.load(real_image).header
-affine = nib.load(real_image).affine
+real_image = nib.load(real_image_path)
+header = nib.load(real_image_path).header
+affine = nib.load(real_image_path).affine
 ### I need to rescale this
 real_image = F.interpolate(real_image, size=(128, 128, 128), mode='trilinear', align_corners=False)
 real_image[real_image != real_image] = 0 #replace nan by 0
